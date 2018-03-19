8cf01bfe518b3167c1db6eb546855a2d2ec50eb2
Marcin Kitowicz <marcin.kitowicz@gmail.com>
2018-03-19 09:00:46 +0100

    Fix issue #4 delete icon remains on that item

--- app/src/main/java/pl/kitek/rvswipetodelete/SwipeToDeleteCallback.kt
+++ app/src/main/java/pl/kitek/rvswipetodelete/SwipeToDeleteCallback.kt
@@ -3,10 +3,14 @@ package pl.kitek.rvswipetodelete
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.PorterDuff
+import android.graphics.PorterDuffXfermode
 import android.graphics.drawable.ColorDrawable
 import android.support.v4.content.ContextCompat
 import android.support.v7.widget.RecyclerView
 import android.support.v7.widget.helper.ItemTouchHelper
+
 
 abstract class SwipeToDeleteCallback(context: Context) : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
 
@@ -15,14 +19,38 @@ abstract class SwipeToDeleteCallback(context: Context) : ItemTouchHelper.SimpleC
     private val intrinsicHeight = deleteIcon.intrinsicHeight
     private val background = ColorDrawable()
     private val backgroundColor = Color.parseColor("#f44336")
+    private val clearPaint = Paint().apply { xfermode = PorterDuffXfermode(PorterDuff.Mode.CLEAR) }
+
+
+    override fun getMovementFlags(recyclerView: RecyclerView?, viewHolder: RecyclerView.ViewHolder?): Int {
+        /**
+         * To disable "swipe" for specific item return 0 here.
+         * For example:
+         * if (viewHolder?.itemViewType == YourAdapter.SOME_TYPE) return 0
+         * if (viewHolder?.adapterPosition == 0) return 0
+         */
+        if (viewHolder?.adapterPosition == 10) return 0
+        return super.getMovementFlags(recyclerView, viewHolder)
+    }
 
     override fun onMove(recyclerView: RecyclerView?, viewHolder: RecyclerView.ViewHolder?, target: RecyclerView.ViewHolder?): Boolean {
         return false
     }
 
-    override fun onChildDraw(c: Canvas?, recyclerView: RecyclerView?, viewHolder: RecyclerView.ViewHolder, dX: Float, dY: Float, actionState: Int, isCurrentlyActive: Boolean) {
+    override fun onChildDraw(
+            c: Canvas?, recyclerView: RecyclerView?, viewHolder: RecyclerView.ViewHolder,
+            dX: Float, dY: Float, actionState: Int, isCurrentlyActive: Boolean
+    ) {
+
         val itemView = viewHolder.itemView
         val itemHeight = itemView.bottom - itemView.top
+        val isCanceled = dX == 0f && !isCurrentlyActive
+
+        if (isCanceled) {
+            clearCanvas(c, itemView.right + dX, itemView.top.toFloat(), itemView.right.toFloat(), itemView.bottom.toFloat())
+            super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
+            return
+        }
 
         // Draw the red delete background
         background.color = backgroundColor
@@ -41,5 +69,9 @@ abstract class SwipeToDeleteCallback(context: Context) : ItemTouchHelper.SimpleC
         deleteIcon.draw(c)
 
         super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
+    }
+
+    private fun clearCanvas(c: Canvas?, left: Float, top: Float, right: Float, bottom: Float) {
+        c?.drawRect(left, top, right, bottom, clearPaint)
     }
 }
